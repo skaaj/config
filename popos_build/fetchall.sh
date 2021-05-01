8c63295e2c107b518b38c7ec40e07b14523fdb7f
Benjamin Denom <ben.denom@gmail.com>
2021-05-01 20:15:36 +0200

    add kitty and todoist

--- popos_build/fetchall.sh
+++ popos_build/fetchall.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env sh
 
 mkdir -p core
+mkdir -p apps
 \cp -r ~/.config/custom ./core/
 \cp ~/.Xresources ./core/.Xresources
 \cp -r ~/.config/rofi/ .
 \cp ~/.zshrc ./core/
+
+mkdir -p apps/todoist
+\cp /opt/Todoist/icon.png ./apps/todoist
+\cp ~/.local/share/applications/todoist.desktop ./apps/todoist/todoist.desktop
+
+mkdir -p apps/kitty
+\cp ~/.config/kitty/* ./apps/kitty
