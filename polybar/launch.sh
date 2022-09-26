d84f4070ad4093dc65b05cddf1e0b1bc851f5d29
Benjamin Denom <ben.denom@gmail.com>
2022-09-26 22:16:49 +0200

    Reset to default polybar config to be up to date

---
+++ polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Launch bar
+echo "---" | tee -a /tmp/polybar1.log
+polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+echo "Bar launched..."
+
